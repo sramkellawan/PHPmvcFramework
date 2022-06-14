f892c9f82cd4174bc0efeaea5880ed4d4687c82f
sramkellawan <102171172+sramkellawan@users.noreply.github.com>
2022-06-14 07:02:03 -0400

    Add files via upload

---
+++ triosmvc/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root'); //your user
+define('DB_PASS', 'syondevin1212'); // your pw
+define('DB_NAME', 'tmvc'); // your dbname
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost/triosmvc');// your URL
+// Site Name
+define('SITENAME', 'TriosMVC'); // your sitename
